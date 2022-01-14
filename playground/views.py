c79b222639bc7baf46ba3250198c842edf85eec5
Kerriea-star <ojared511@gmail.com>
2022-01-14 10:52:35 +0300

    Started a django strorefront project

---
+++ playground/views.py
@@ -0,0 +1,6 @@
+from curses.ascii import HT
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def say_hello(request):
+    return render(request, 'hello.html', {'name': 'Mosh'})
